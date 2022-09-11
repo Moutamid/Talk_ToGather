b975b63b6a726278db276dbbed0719d5d77e49c4
Moutamid <lumianokia188@gmail.com>
2022-09-11 08:33:41 +0500

    Bug Fixed

--- app/src/main/java/com/moutamid/talk_togather/Notifications/MyFirebaseMessaging.java
+++ app/src/main/java/com/moutamid/talk_togather/Notifications/MyFirebaseMessaging.java
@@ -38,8 +38,8 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
 
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         SharedPreferencesManager prefs = new SharedPreferencesManager(getApplicationContext());
-        boolean notify = prefs.retrieveBoolean("isNotify",false);
-        if(firebaseUser != null && sent.equals(firebaseUser.getUid())){
+        boolean notify = prefs.retrieveBoolean("isNotify", false);
+        if (firebaseUser != null && sent.equals(firebaseUser.getUid())) {
             if (!notify) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     sendOreoNotification(remoteMessage);
@@ -51,7 +51,7 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    private void sendOreoNotification(RemoteMessage remoteMessage){
+    private void sendOreoNotification(RemoteMessage remoteMessage) {
         String user = remoteMessage.getData().get("user");
         String icon = remoteMessage.getData().get("icon");
         String title = remoteMessage.getData().get("title");
@@ -59,28 +59,29 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
 
         RemoteMessage.Notification notification = remoteMessage.getNotification();
 
-        int j =  Integer.parseInt(user.replaceAll("[\\D]", ""));
+        int j = Integer.parseInt(user.replaceAll("[\\D]", ""));
         Intent intent;
         intent = new Intent(this, DashBoard.class);
         Bundle bundle = new Bundle();
-        bundle.putString("userId",user);
-        bundle.putBoolean("isFromPushNotif",true);
+        bundle.putString("userId", user);
+        bundle.putBoolean("isFromPushNotif", true);
 
         intent.putExtras(bundle);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,j,intent,PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, j, intent,
+                PendingIntent.FLAG_IMMUTABLE);
 
         Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         OreoNotification oreoNotification = new OreoNotification(this);
         Notification.Builder builder = null;
-     //   if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            builder = oreoNotification.getOreoNotification(title, body, pendingIntent, defaultSound, icon);
-       // }
+        //   if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        builder = oreoNotification.getOreoNotification(title, body, pendingIntent, defaultSound, icon);
+        // }
 
         int i = 0;
-        if(j > 0){
+        if (j > 0) {
             i = j;
         }
 
@@ -88,7 +89,7 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
     }
 
     @SuppressWarnings("deprecation")
-    private void sendNotification(RemoteMessage remoteMessage){
+    private void sendNotification(RemoteMessage remoteMessage) {
         String user = remoteMessage.getData().get("user");
         String icon = remoteMessage.getData().get("icon");
         String title = remoteMessage.getData().get("title");
@@ -96,17 +97,25 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
 
         RemoteMessage.Notification notification = remoteMessage.getNotification();
 
-        int j =  Integer.parseInt(user.replaceAll("[\\D]", ""));
+        int j = Integer.parseInt(user.replaceAll("[\\D]", ""));
 
         Intent intent = new Intent(this, DashBoard.class);
         Bundle bundle = new Bundle();
-        bundle.putString("userId",user);
+        bundle.putString("userId", user);
         intent.putExtras(bundle);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,j,intent,PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingIntent = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            pendingIntent = PendingIntent.getActivity(this, j, intent,
+                    PendingIntent.FLAG_IMMUTABLE);
+        }else {
+            pendingIntent = PendingIntent.getActivity(this, j, intent,
+                    PendingIntent.FLAG_ONE_SHOT);
+        }
+//                PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder builder =  new NotificationCompat.Builder(this)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setSmallIcon(Integer.parseInt(icon))
                 .setContentTitle(title)
                 .setContentText(body)
@@ -114,10 +123,10 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
                 .setSound(defaultSound)
                 .setContentIntent(pendingIntent);
 
-        NotificationManager noti = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager noti = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         int i = 0;
-        if(j > 0){
+        if (j > 0) {
             i = j;
         }
 
